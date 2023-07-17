cf66429c0974594bd26d87f85bdd6e4fdea25690
A-Rush <83367414+Ara-13@users.noreply.github.com>
2023-07-17 22:26:20 +0330

    Add files via upload

---
+++ GuessNumber.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] arg){
+        Scanner inputReader = new Scanner(System.in);
+        int randomNum = (int)(Math.random()*100);
+        int userNum = 0;
+        int attempts = 0;
+        while(userNum != 999){
+            System.out.println("Guess A Number Between 0 to 100: ");
+            int Test = Integer.parseInt(inputReader.nextLine());
+            if(Test < 0 || Test > 100){
+                System.out.println("You Should Guess Between 0 to 100! ");
+            }else {
+                userNum = Test;
+                if(userNum < randomNum){
+                    System.out.println("Guess Higher!");
+                    ++attempts;
+                }else if(userNum > randomNum){
+                    System.out.println("Guess Lower!");
+                    ++attempts;
+                }else{
+                    ++attempts;
+                    userNum = 999;
+                    System.out.println("You Guessed Correctly!\nWith: " + attempts + " Attempts");
+                }
+            }
+        }
+
+
+    }
+}
